be60efaed251967a6f2194e8873de8727b4ad4f9
Mihai Tomescu <matomesc@gmail.com>
2014-08-21 19:30:12 -0700

    fixed `findRandom` to have the exact same signature as `Model.find` + tests

--- index.js
+++ index.js
@@ -1,51 +1,113 @@
-module.exports = function (options) {
-  var randFn = (options && options.fn) || Math.random;
-  var randCoords = function () { return [randFn(), randFn()] }
+/**
+ * @method random
+ * @param {mongoose.Schema} schema
+ * @param {Object}          options
+ * @param {Function}        [options.fn=Math.random]
+ * @param {String}          [options.path='random']
+ */
+function random(schema, options) {
+	options = options || {};
+	var randFn = options.fn || Math.random;
+	var randCoords = function () { return [randFn(), randFn()] }
+	var path = options.path || 'random';
+	var field = {};
+	field[path] = {
+		type: { type: String, default: 'Point' },
+		coordinates: { type: [Number], default: randCoords }
+	};
+	var index = {};
+	index[path] = '2dsphere';
 
-  function random(schema, options) {
-    var path = (options && options.path) || 'random';
-    var field = {};
-    field[path] = {
-      type: { type: String, default: 'Point' },
-      coordinates: { type: [Number], default: randCoords }
-    };
-    var index = {};
-    index[path] = '2dsphere';
+	schema.add(field);
+	schema.index(index);
 
-    schema.add(field);
-    schema.index(index);
+	/**
+	 * @method findRandom
+	 * @param {Object}    conditions
+	 * @param {Object}    [fields]
+	 * @param {Object}    [options]
+	 * @param {Function}  [callback]
+	 */
+	schema.statics.findRandom = function (conditions, fields, options, callback) {
+		var self = this;
 
-    schema.statics.findRandom = function (query, callback) {
-      var self = this;
+		if (typeof conditions === 'function') {
+			conditions = {};
+		}
 
-      if (typeof query === 'function') {
-        callback = query;
-        query = {};
-      }
+		conditions[path] = conditions[path] || {
+			$near: {
+				$geometry: { type: 'Point', coordinates: randCoords() }
+			}
+		};
 
-      query[path] = query[path] || {
-        $near: {
-          $geometry: { type: 'Point', coordinates: randCoords() }
-        }
-      };
+		var query = self.find.call(self, conditions, fields, options, callback);
+		query.__random = { path: path, query: conditions[path] };
+		return query;
+	};
 
-      self.findOne(query, function (err, doc) {
-        if (err) return callback(err);
-        callback(null, doc);
-      });
-    };
+	/**
+	 * @method syncRandom
+	 * @param  callback
+	 */
+	schema.statics.syncRandom = function (callback) {
+		var self = this;
+		var stream = self.find({}).stream({ transform: transform });
+		var result = {
+			attempted: 0,
+			updated: 0
+		};
+		var left = 0;
+		var streamEnd = false;
 
-    schema.statics.syncRandom = function (callback) {
-      var self = this;
-      self.find({}, function (err, docs) {
-        if (typeof callback === 'function' && err) return callback(err);
-        docs.forEach(function(doc) {
-          self.update({ _id: doc._id }, { $set: { random: { coordinates: randCoords(), type: 'Point' } } }).exec();
-        });
-        if (typeof callback === 'function') callback(null);
-      });
-    }
-  }
+		stream.on('data', function (doc) {
+			result.attempted += 1;
+			left += 1;
+			doc.save(function (err) {
+				if (err) {
+					console.error(err.stack);
+				} else {
+					result.updated += 1;
+				}
 
-  return random;
+				left -= 1;
+				if (streamEnd && !left) {
+					return callback(null, result);
+				}
+			});
+		}).on('error', function (err) {
+			console.error(err.stack);
+		}).on('end', function () {
+			streamEnd = true;
+		});
+		return stream;
+	};
+
+	function transform(doc) {
+		var update = {
+			type: 'Point',
+			coordinates: randCoords()
+		};
+		doc.set(path, update);
+		return doc;
+	}
+}
+module.exports = random;
+
+/**
+ * Merges keys from `objects` into `base`, without overwriting.
+ *
+ * @method  merge
+ * @param   {Object}    base
+ * @param   {[Object]}  [objects]*
+ * @returns {Object}
+ */
+function merge(base) {
+	var objects = [].slice.call(arguments, 1);
+	objects.forEach(function (obj) {
+		Object.keys(obj).forEach(function (key) {
+			base[key] = base[key] === undefined ? obj[key] : base[key];
+		});
+	});
+	return base;
 }
---
+++ spec/index_spec.js
@@ -0,0 +1,106 @@
+var assert = require('assert');
+var mongoose = require('mongoose');
+var random = require('../index');
+
+mongoose.connect('mongodb://localhost/test');
+
+var Schema = new mongoose.Schema({}, { strict: false });
+Schema.plugin(random, { path: 'r' });
+var Test = mongoose.model('Test', Schema);
+
+function dist(a, b) {
+	return Math.sqrt(
+			Math.pow(b[0] - a[0], 2) +
+			Math.pow(b[1] - a[1], 2)
+	);
+}
+
+describe('Model#findRandom()', function () {
+	afterEach(function (done) {
+		Test.remove({}, function (err) {
+			assert.ifError(err);
+			done();
+		});
+	});
+
+	it('should return a usable `mongoose.Query` object', function (done) {
+		var docs = [{
+			name: 'bob',
+			r: {
+				coordinates: [0.95, 0.95]
+			}
+		}, {
+			name: 'bobby',
+			r: {
+				coordinates: [0.90, 0.95]
+			}
+		}, {
+			name: 'billy',
+			r: {
+				coordinates: [0.85, 0.95]
+			}
+		}];
+
+		docs = docs.map(function (d) {
+			return new Test(d);
+		});
+
+		var bob = docs[0];
+		var bobby = docs[1];
+		var billy = docs[2];
+
+		Test.create(docs, function (err) {
+			if (err) {
+				return done(err);
+			}
+			var query = Test.findRandom({ name: /b.*/ig }, { name: 0 });
+			query.limit(2).exec(function (err, returnDocs) {
+				assert.ifError(err);
+				assert(returnDocs.length === 2);
+				assert(query instanceof mongoose.Query);
+
+				returnDocs.forEach(function (doc) {
+					assert(doc.name === undefined);
+				});
+
+				done();
+			});
+		});
+	});
+
+  it('should return the closest document', function (done) {
+    var docs = [];
+    var coords, d;
+
+    for (var i = 0; i < 100; i++) {
+	    coords = [Math.random(), Math.random()];
+      d = dist(coords, [1, 1]);
+      docs.push({ r: { type: 'Point', coordinates: coords }, dist: d });
+    }
+
+    Test.create(docs, function (err) {
+      assert.ifError(err);
+      Test.findRandom({
+        r: {
+          $near: {
+            $geometry: { type: 'Point', coordinates: [1, 1] }
+          }
+        }
+      }, {}, { limit: 1 }, function (err, actual) {
+        assert.ifError(err);
+        Test.findOne().sort('dist').exec(function (err, expected) {
+	        console.log('actual:', actual);
+	        console.log('expected:', expected);
+          assert.ifError(err);
+          assert(actual[0].get('r.coordinates.0') === expected.get('r.coordinates.0'));
+          assert(actual[0].get('r.coordinates.1') === expected.get('r.coordinates.1'));
+
+	        Test.remove({}, function (err) {
+		        assert.ifError(err);
+		        done();
+	        });
+        });
+      });
+    });
+  });
+});
